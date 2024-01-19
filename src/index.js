cbf71e3df9783b8ab5ae432990827bf562f235a7
Harsh kasana <bt22ece106@gmail.com>
2024-01-19 10:44:48 +0530

    SideBar Navbar Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-
+import "./index.css";
 import App from "./App";
+import { ContextProvider, useStateContext } from "./contexts/ContextProvider";
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+
+ReactDOM.render(
+    <ContextProvider>
+        <App/>
+    </ContextProvider>
+    ,document.getElementById('root'));
